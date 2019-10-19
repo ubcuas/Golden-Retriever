95f39f6e2cd55e5546f1378741298def5e5403ed
Cindy <cindyxmiao@gmail.com>
2019-10-20 00:53:57 +0200

    done

---
+++ app.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import time
+
+import requests
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+PATH_TO_WRITE = '/home/claire/UAS/gcom-x/backend/mediafiles/images'
+NGINX_URL = 'http://172.0.0.17/images/'
+EXT1 = 'JPEG'
+EXT2 = 'jpg'
+wait = 1
+
+while True:
+
+
+    def check_status():
+        retries = 0
+        success = False
+        while not success:
+            try:
+                urlopen(NGINX_URL, timeout=1)
+                success = True
+            except Exception as e:
+                retries += 1
+                print('Error! Waiting %s secs and re-trying...' % wait, flush=True)
+                print(retries, flush=True)
+                time.sleep(wait)
+
+
+    def list_of_imgs(url):
+        check_status()
+        page = requests.get(url).text
+        # print(page)
+        soup = BeautifulSoup(page, 'html.parser')
+        return [url + '/' + node.get('href') for node in soup.find_all('a') if (node.get('href').endswith(EXT1)
+                                                                                | node.get('href').endswith(EXT2))]
+
+
+    for file in list_of_imgs(NGINX_URL):
+        img_name = file.split('/')[-1]
+        absolute_path = os.path.join(PATH_TO_WRITE, img_name);
+
+        if not os.path.exists(absolute_path):
+            # print(file)
+            img_data = requests.get(file).content
+
+            with open(os.path.join(PATH_TO_WRITE, img_name), 'wb') as handler:
+                handler.write(img_data)
+    time.sleep(1)
